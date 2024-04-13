38b4f3b46652643cfbae3c46a2e32741e5c85b00
unknown <scopeelius@gmail.com>
2024-04-13 20:53:56 +0300

    Doctor Log In

---
+++ lib/screens/loginSelection.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginSelection extends StatefulWidget {
+  const LoginSelection({super.key});
+
+  @override
+  State<LoginSelection> createState() => _LoginSelectionState();
+}
+
+class _LoginSelectionState extends State<LoginSelection> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
